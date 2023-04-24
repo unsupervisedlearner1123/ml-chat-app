289b7b0867725f8a8864789cc5fc72e3076e9452
Deekshita Saikia <68491812+unsupervisedlearner1123@users.noreply.github.com>
2023-04-24 22:57:05 +0000

    added timestamp

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    date = datetime.now()
+    return '@' + str(date)+ ': Hello, World!'
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=8080)
