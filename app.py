7ca1198d1a36ab1ad70703a0c636f5e9654784b1
Satvik Kishore <satvik.kishore@gmail.com>
2023-04-25 16:45:38 -0400

    wrapper to chat

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from datetime import datetime
+import chatbot
 
 app = Flask(__name__)
 
@@ -7,6 +8,12 @@ app = Flask(__name__)
 def hello_world():
     date = datetime.now()
     return '@' + str(date)+ ': Hello, World!'
+
+@app.route('/chat')
+def otherroute():
+    # date = datetime.now()
+    val = chatbot.fun()
+    return val
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=8080)
